3400ef6fc1ac0dc7884ecd59530c0f6203726dec
sunShineLoveMe <juneandrao@outlook.com>
2023-07-10 16:57:48 +0800

    品种合约

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/期货品种3D可视化动态模型曲线/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -9,3 +9,4 @@ declare module "*.css" {
     const css: { [key: string]: string };
     export default css;
 }
+
