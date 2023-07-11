976a556c04bf44c05991037ebd8ab3f34ebaccdd
sunShineLoveMe <juneandrao@outlook.com>
2023-07-11 15:52:34 +0800

    构建左侧菜单项

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Layout } from 'antd';
 import styles from "./App.module.css"
+import { Menu } from './components';
 
 function App() {
   return (
     <div className={styles.App}>
       <Layout className={styles["global-layout"]}>
-        <Layout.Sider className={styles["global-sider"]}>垂直菜单</Layout.Sider>
+        <Layout.Sider className={styles["global-sider"]} width={256}>
+          <Menu />
+        </Layout.Sider>
         <Layout className={styles["content-layout"]}>
           <Layout.Header className={styles["layout-header"]}>头部</Layout.Header>
           <Layout.Content className={styles["layout-content"]}>内容</Layout.Content>
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import styles from './Menu.module.css'
+import { CodeSandboxOutlined, MailOutlined, SettingOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu as AntdMenu } from "antd";
+
+// 这段代码主要是标识menuProps对象中items属性的值的类型，即数组中任意元素的类型
+type MenuItem = Required<MenuProps>['items'][number];
+
+/**
+ * 
+ * @param label 菜单项标题
+ * @param key item的唯一标识
+ * @param icon 菜单项图标
+ * @param children 菜单项的子菜单项
+ * @param type 菜单项类型
+ * @returns 
+ */
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+): MenuItem {
+    return {
+        key,
+        label,
+        icon,
+        children,
+        type,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Navigation One', 'sub1', <CodeSandboxOutlined />, [
+        getItem('Option 1', '1'),
+        getItem('Option 2', '2'),
+        getItem('Option 3', '3'),
+        getItem('Option 4', '4'),
+    ]),
+    getItem('Navigation Two', 'sub2', <CodeSandboxOutlined />, [
+        getItem('Option 5', '5'),
+        getItem('Option 6', '6'),
+        getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+    ]),
+    getItem('Navigation Three', 'sub4', <CodeSandboxOutlined />, [
+        getItem('Option 9', '9'),
+        getItem('Option 10', '10'),
+        getItem('Option 11', '11'),
+        getItem('Option 12', '12'),
+    ]),
+]
+
+// submenu keys of first level
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+
+export const Menu: React.FC = () => {
+
+    // 当前展开的subMenu菜单项 key 数组 string[]
+    const [openKeys, setOpenKeys] = useState(['sub1']);
+
+    // subMenu展开或关闭的回调
+    // 表示将onOpenChange定义为一个函数类型，它接受一个字符串数组类型的参数
+    const onOpenChange: MenuProps['onOpenChange'] = keys => {
+        const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
+        if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
+    return (
+        <AntdMenu
+            mode="inline"
+            openKeys={openKeys}
+            onOpenChange={onOpenChange}
+            style={{ width: 256 }}
+            items={items}
+            theme="dark"
+        />
+    )
+}
