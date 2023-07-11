93441952a9ce7681cecc030c6281a4fa16916a7b
sunShineLoveMe <juneandrao@outlook.com>
2023-07-11 15:55:02 +0800

    菜单选项

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -39,11 +39,6 @@ const items: MenuItem[] = [
         getItem('Option 3', '3'),
         getItem('Option 4', '4'),
     ]),
-    getItem('Navigation Two', 'sub2', <CodeSandboxOutlined />, [
-        getItem('Option 5', '5'),
-        getItem('Option 6', '6'),
-        getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-    ]),
     getItem('Navigation Three', 'sub4', <CodeSandboxOutlined />, [
         getItem('Option 9', '9'),
         getItem('Option 10', '10'),
